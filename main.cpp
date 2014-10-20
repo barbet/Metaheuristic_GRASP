2f128d2f6136007ceebb6dc600aaa5a8eedaa700
barbet <tristan.barbe@wanadoo.fr>
2014-10-20 20:46:06 +0200

    commenter les omp.h

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>   // Standard Input/Output Header
 #include <stdlib.h>  // C Standard General Utilities Library
-#include <omp.h>     // Open Multi-Processing Library
+//#include <omp.h>     // Open Multi-Processing Library
 #include <algorithm>
 #include <iostream>
 #include "testio.h"  // Test Input/Output Header
@@ -20,13 +20,13 @@ void Test_Tristan()
 void Test_Etienne()
 {
   Testio myTestio("TestCases/Input/cap71.txt");
-  printf("Nombre de processeurs : %d \n",omp_get_num_procs());
+  /*printf("Nombre de processeurs : %d \n",omp_get_num_procs());
   printf("Nombre de thread actifs : %d \n",omp_get_num_threads());
   int n;
   #pragma omp parallel for schedule(dynamic,2)
   for(n=0;n<18;n++){
     printf("Element %d traité par le thread %d \n",n,omp_get_thread_num());
-  }
+  }*/
 }
 
 
